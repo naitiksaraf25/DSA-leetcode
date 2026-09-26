1038a5c190b51c5fc4ac01a55f6a4a55b7bdd040
Naitik Saraf <naitiksaraf2507@gmail.com>
2026-09-26 13:27:51 +0530

    Time: 167 ms (90.75%), Space: 110.6 MB (69.46%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -4,23 +4,32 @@ public:
         int ans = 0;
         while (num > 0) {
             int x = num % 10;
-            ans = (ans*10)+x;
-            num = num/10;
+            ans = (ans * 10) + x;
+            num = num / 10;
         }
         return ans;
     }
     int countDistinctIntegers(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int>count;
-        for (int i = 0; i < nums.size(); i++) {
-            int x = solve(nums[i]);
-            count.push_back(x);
+        unordered_set<int> st;
+
+        for (int num : nums) {
+            st.insert(num);
+            st.insert(solve(num));
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-            mp[count[i]]++;
-        }
-        return mp.size();
+
+        return st.size();
+
+        // unordered_map<int, int> mp;
+        // vector<int>count;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int x = solve(nums[i]);
+        //     count.push_back(x);
+        // }
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     mp[nums[i]]++;
+        //     mp[count[i]]++;
+        // }
+        // return mp.size();
     }
 };
